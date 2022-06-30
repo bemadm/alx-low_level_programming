dcd0190300fa6f5d5a9153115058814303e989fa
bemadm <bemnetadmasu9@gmail.com>
2022-06-30 04:43:34 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ * main - starts the programme to print.
+ * function puts() - print exactly what is inside it.
+ * return() - it exit properly return a value 1.
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+*main - starts program
+*function main - print exactly the message
+*return - if exited properly return zero
+*/
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
