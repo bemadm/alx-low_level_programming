6f91662dc01da85e83003f8606c412a028a5684e
bemadm <bemnetadmasu9@gmail.com>
2022-06-30 15:15:38 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
-
+#include<errno.h>
 /**
 * main - prints exactly and that piece of art is useful"
 * - Dora Korpar, 2015-10-19 .
@@ -10,6 +10,6 @@
 */
 int main(void)
 {
-write(1, "and that piece of art is usefull\" -Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is usefull\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
