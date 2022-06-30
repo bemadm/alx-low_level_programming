8a6c718ad886b6239cecfc644c9fc34f86a53171
bemadm <bemnetadmasu9@gmail.com>
2022-06-30 13:05:31 -0700

    hot fix task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 /**
-*main print exactly the messagewith proper grammar, 
-but the outcome is a piece of art,
-*Return : always 0
+* main -entry point
+* Return: Always 0 (sucess)
 */
 int main(void)
 {
