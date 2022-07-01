28d9df784f6f7af02203cd9731ae4e049cc11853
bemadm <bemnetadmasu9@gmail.com>
2022-07-01 11:19:26 -0700

    hot fix

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main-prints all the numbers of base 16 in lowercase, followed by a new line.
+* Return: Always 0 (sucess)
+*/
+int main(void)
+{
+int ch;
+int n;
+for (ch = 48; ch <= 57; ch++)
+{
+for (n = 48; n <= 57; n++)
+{
+putchar(ch);
+putchar(n);
+if (ch != 57 || n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar(10);
+return (0);
+}
