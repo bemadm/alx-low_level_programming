fdfa4105f65df39b378fdedf5baddeff0bc4f12e
bemadm <bemnetadmasu9@gmail.com>
2022-07-05 13:57:05 -0700

    task1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,23 @@
 #include<unistd.h>
 /**
- * -puchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  *  @c : the character to print
  *  Return : on success 1
  *  on error, -1 is returned, and err no is set appropritely
  */
-int -putchar(char c)
+int _putchar(char c);
 {
-	return (write(1, &c, 1);
-}
+	return (write(1, &c, 1));
+			
+#include<unistd.h>                                                
+
+/**
+* _putchar - writes the character c to stdout
+*  @c : the character to print                                   
+*  Return : on success 1                                         
+*on error, -1 is returned, and err no is set appropritely    
+*/                                                               
+int _putchar(char c);                                             
+{
+return(write(1, &c, 1));
+}}	
