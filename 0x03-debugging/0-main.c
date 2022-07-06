3a8231c89e9b46f20510dfd0affde52d06e158ab
bemadm <bemnetadmasu9@gmail.com>
2022-07-06 13:53:24 -0700

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
  * main - finds and prints the first 98 Fibonacci numb
  * Return: ALways 0 (Success)
  */
+
 int main(void)
 {
 int i;
+
 i = 0;
 postive_or_negative(i);
-	return (0);
+
+return (0);
 }
 
