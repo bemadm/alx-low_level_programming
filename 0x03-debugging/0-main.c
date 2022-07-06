4b4a7bb1f57c5440b49791fe82026b2b6d605bb9
bemadm <bemnetadmasu9@gmail.com>
2022-07-06 13:24:05 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - finds and prints the first 98 Fibonacci numb
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = 0;
+postive_or_negative(i);
+	return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdio.h>
+void postive_or_negative(int i);
+#endif
