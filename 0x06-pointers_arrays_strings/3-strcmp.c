3b44b5ee2a41b3f618505b2a574e233c23e4ad7e
bemadm <bemnetadmasu9@gmail.com>
2022-07-13 10:00:33 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp -> this a function for comparing purpose
+* @s1 : first parameter
+* @s2 : second parameter
+* Return: integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+int result;
+
+result = strncpy(s1, s2);
+return (result);
