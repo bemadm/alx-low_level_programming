8741b1fef596b40c33464f9293725f174f51ec74
bemadm <bemnetadmasu9@gmail.com>
2022-07-22 13:45:16 -0700

    fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,14 +5,13 @@
 * @argv : second parameter
 * Return: 0 success
 */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-printf("%d\n", argc);
+
 int i;
 for (i = 0; i < argc; i++)
 {
-printf("argv index %d is = %s\n", i, argv[i]);
+printf(" %s\n", argv[i]);
 }
-printf("argv index %d is = %s\n", argc, argv[argc]);
 return (0);
 }
