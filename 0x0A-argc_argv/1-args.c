7f211104d0cd6052f47b40173358c4ddd27af579
bemadm <bemnetadmasu9@gmail.com>
2022-07-22 13:28:25 -0700

    hot fix task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
-#include <stdio.h>   
-/**                                                                 *main : this is a function to print its name                    
-* @argc : argc paramet
-* @argv: an array of a command liste
-* Rreturn : 0 for success
+#include <stdio.h>
+/**
+* main -> this a function strcat
+* @argc : first parameter
+* @argv : second parameter
+* Return: 0 success
 */
-int main(int argc, char *argv[] __attribute__((unused)))         
-{                                                                  
-printf("%d\n", argc -1);                           
-return (0);                                                 
-} 
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
